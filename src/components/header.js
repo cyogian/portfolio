cd1b5042fd789264add6a7bc94de7974fc0089f5
Amar Nath Yogi <immortal2907@gmail.com>
2020-08-03 00:02:04 +0530

    sidebar created, social icons added

--- src/components/header.js
+++ src/components/header.js
@@ -6,34 +6,45 @@ import Social from "./social"
 
 import classes from "./header.module.scss"
 
-const Header = props => (
-  <div className={classes.Header}>
-    <span className={classes.Logo}>Cyogian</span>
-    <div className={classes.Menu}>
-      <div className={classes.LeftMenu}>
-        <NavLinks className={classes.NavBar} activeClassName={classes.Active} />
-      </div>
-      <div className={classes.RightMenu}>
-        <a className={classes.Resume} href="/" download>
-          <i className="fas fa-file-download"></i>
-        </a>
-        <div
-          className={classes.Toggle}
-          onClick={props.onClickToggle}
-          role="button"
-          tabIndex="-5"
-          aria-pressed="false"
-          aria-expanded={props.status}
-          onKeyDown={props.onClickToggle}
-        >
-          <i className="fas fa-bars"></i>
+const Header = props => {
+  let { Toggle } = classes
+  if (props.status) {
+    Toggle = `${classes.Toggle} ${classes.Open}`
+  }
+  return (
+    <div className={classes.Header}>
+      <span className={classes.Logo}>Cyogian</span>
+      <div className={classes.Menu}>
+        <div className={classes.LeftMenu}>
+          <NavLinks
+            className={classes.NavBar}
+            activeClassName={classes.Active}
+          />
         </div>
+        <div className={classes.RightMenu}>
+          <a className={classes.Resume} href="/" download>
+            <i className="fas fa-file-download"></i>
+          </a>
+          <div style={{ width: "2rem" }}>
+            <div
+              className={Toggle}
+              onClick={props.onClickToggle}
+              role="button"
+              tabIndex="-5"
+              aria-pressed="false"
+              aria-expanded={props.status}
+              onKeyDown={props.onClickToggle}
+            >
+              <i className={props.status ? "fas fa-times" : "fas fa-bars"}></i>
+            </div>
+          </div>
 
-        <Social className={classes.Social} />
+          <Social className={classes.Social} />
+        </div>
       </div>
     </div>
-  </div>
-)
+  )
+}
 
 Header.propTypes = {
   onClickToggle: PropTypes.func.isRequired,
--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,9 +1,22 @@
 import React from "react"
 import PropTypes from "prop-types"
 
+import Social from "./social"
+import NavLinks from "./navlinks"
 import classes from "./sidebar.module.scss"
 
-const Sidebar = props => <div className={classes.Sidebar}>Sidebar</div>
+const Sidebar = props => {
+  let { Sidebar } = classes
+  if (props.show) {
+    Sidebar = `${Sidebar} ${classes.Open}`
+  }
+  return (
+    <div className={Sidebar}>
+      <NavLinks className={classes.NavBar} activeClassName={classes.Active} />
+      <Social className={classes.Social} />
+    </div>
+  )
+}
 
 Sidebar.propTypes = {
   show: PropTypes.bool,
