0cdb636662e332fc037086ddc64622fbaa86f6c4
Amar Nath Yogi <immortal2907@gmail.com>
2020-08-06 01:55:18 +0530

    Home Page Modified

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,21 @@ import React, { Component } from "react"
 import Layout from "../components/layout"
 import Head from "../components/head"
 
+import classes from "../styles/index.module.scss"
+
 class Index extends Component {
   render() {
     return (
       <Layout>
         <Head title="Home" />
-        Home
+        <div className={classes.Index}>
+          <div className={classes.Computer}>
+            <img src="computerVector.svg" alt="computerBackgroundimage" />
+            <div className={classes.Screen}>
+              Hello, I'm Amar Nath Yogi.<span>|</span>
+            </div>
+          </div>
+        </div>
       </Layout>
     )
   }
