6c8b67053ee15ac48363c57bf31f588c30ea84d7
Amar Nath Yogi <immortal2907@gmail.com>
2020-08-06 18:27:14 +0530

    Home Page Completed

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,6 +2,10 @@ import React from "react"
 
 import classes from "./footer.module.scss"
 
-const Footer = () => <div className={classes.Footer}>Footer</div>
+const Footer = () => (
+  <div className={classes.Footer}>
+    Copyright © {new Date().getFullYear()} Amar Nath Yogi. All Rights Reserved
+  </div>
+)
 
 export default Footer
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react"
+import Typewriter from "typewriter-effect"
 
 import Layout from "../components/layout"
 import Head from "../components/head"
 
 import classes from "../styles/index.module.scss"
+import { Link } from "gatsby"
 
 class Index extends Component {
   render() {
@@ -13,8 +15,21 @@ class Index extends Component {
         <div className={classes.Index}>
           <div className={classes.Computer}>
             <img src="computerVector.svg" alt="computerBackgroundimage" />
-            <div className={classes.Screen}>
-              Hello, I'm Amar Nath Yogi.<span>|</span>
+            <div className={classes.Screen} id="Screen">
+              <Typewriter
+                options={{
+                  strings: [
+                    "Hello, I'm Amar.<br/>I'm a Full Stack Web Developer.",
+                    "I build Web Apps using Javascript & Python.",
+                    "I am proficient in building both Frontend & Backend of a WebApp.",
+                    "I can make Multi-Page & Single-Page Progressive Web Applications.",
+                    "I can build ReST APIs using Python/Flask & Node.js/Express.js.",
+                  ],
+                  autoStart: true,
+                  loop: true,
+                }}
+              />
+              <Link to="/technologies">Click Here to Explore My Skill Set</Link>
             </div>
           </div>
         </div>
