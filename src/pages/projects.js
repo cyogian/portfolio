9189f6c93a7e23bef26785e7dc0c6478e8db3179
Amar Nath Yogi <immortal2907@gmail.com>
2020-09-03 23:40:39 +0530

    added Projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -3,12 +3,14 @@ import React, { Component } from "react"
 import Layout from "../components/layout"
 import Head from "../components/head"
 
+import classes from "../styles/projects.module.scss"
+
 class Projects extends Component {
   render() {
     return (
       <Layout>
         <Head title="Projects" />
-        Projects
+        <div className={classes.Projects}></div>
       </Layout>
     )
   }
